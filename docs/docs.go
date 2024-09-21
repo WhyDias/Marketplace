0e6aae7ab3d7c65d7c4617a9698c1d50cd430615
Dias Maidanbekov <67271427+WhyDias@users.noreply.github.com>
2024-09-22 00:06:46 +0500

    add to swagger docs ip

--- docs/docs.go
+++ docs/docs.go
@@ -524,7 +524,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "195.49.215.120:8080",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Marketplace API",
