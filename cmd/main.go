3fdba25f3752ec909d8a65b48e0f8744690740b9
Dias Maidanbekov <67271427+WhyDias@users.noreply.github.com>
2024-10-05 14:47:27 +0500

    add

--- cmd/main.go
+++ cmd/main.go
@@ -102,8 +102,8 @@ func main() {
 		authorized.GET("/api/products/moderated", productController.GetModeratedProducts)
 		authorized.GET("/api/products/unmoderated", productController.GetUnmoderatedProducts)
 		authorized.POST("/api/categories", supplierController.AddCategory)
-		authorized.POST("/api/products", productController.AddProduct)
-		//authorized.POST("/api/categories/attributes", categoryController.AddCategoryAttributes)
+		//authorized.POST("/api/products", productController.AddProduct)
+		authorized.POST("/api/categories/attributes", categoryController.AddCategoryAttributes)
 		authorized.GET("/api/categories/:id/attributes", categoryController.GetCategoryAttributesByCategoryID)
 	}
 
